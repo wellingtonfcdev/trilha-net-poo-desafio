1b1ebafb002e18e9dda65b6801bf557a40d8d308
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-16 09:55:16 -0300

    Implementacao desafio classe abstrata

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone Nokia = new Nokia(numero: "922223333", modelo:"Nokia001",imei:"1023210",memoria: 64);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Meeting");
+Nokia.ReceberLigacao();
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero:"988884444", modelo:"2432", imei:"30423", memoria: 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Ifood");
+iphone.ReceberLigacao();
